e153fb422249346a44ec5f2007d5e426bcd2f4bb
raynnanchahal <raynnanchahal@gmail.com>
2026-03-16 14:39:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/",
+export default defineConfig({
+  base: "/neuroads-casecraft/",
+
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+
+  plugins: [
+    react(),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
